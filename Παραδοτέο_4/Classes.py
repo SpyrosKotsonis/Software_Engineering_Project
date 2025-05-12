1d9c84f8e510f43071980517e15161af90ca0e75
Spyros Kotsonis <spiro.kotsonis@gmail.com>
2025-05-12 21:01:04 +0300

    User Class

--- Παραδοτέο_4/Classes.py
+++ Παραδοτέο_4/Classes.py
@@ -4,3 +4,16 @@ class Log_Entry:
     User = "Spyros"
     request = "GET"
     Action = ""
+
+class User:
+    user_id = 0 
+    Name = ""
+    Email = ""
+    def __init__(id, name, email):
+        user_id = id
+        Name = name 
+        Email = email
+
+class Request:
+    request_id = 0
+    user = User(1100596, "Spyros", "spiro.kotsonis@gmail.com") 
