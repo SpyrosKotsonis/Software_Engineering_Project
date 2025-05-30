f47913f9f4d7419d0da33a73e3b78d91032eb3e4
Spyros Kotsonis <spiro.kotsonis@gmail.com>
2025-05-30 09:22:09 +0300

    temp classes, no functionality

--- Παραδοτέο_4/Classes.py
+++ Παραδοτέο_4/Classes.py
@@ -1,22 +1,15 @@
-class Log_Entry:
-    log_id = 0
-    TimeStamp = 0
-    User = "Spyros"
-    request = "GET"
-    Action = ""
-
-class User:
+class Account:
     def __init__(self, id, name, email):
         self.user_id = id
         self.Name = name 
         self.Email = email
 
-class Employee(User):
+class User(Account):
     def __init__(self, id, name, email, position):
         super().__init__(id, name, email)
         self.position = position
 
-class Admin(User):
+class Admin(Account):
     def __init__(self, id, name, email):
         super().__init__(id, name, email)
 
@@ -38,4 +31,78 @@ class Request:
 class Repair_Request(Request):
     def __init__(self, id, user, equipment, date, status, repair_details):
         super().__init__(id, user, equipment, date, status)
-        self.repair_details = repair_details
+        self.repair_details = repair_details        
+
+class Equipment_Queue:
+    def __init__(self, equipment_id):
+        self.equipment = equipment_id
+        self.User_Order = []  # List to hold user IDs in the queue
+        
+    def Add_User_to_Queue(self, user_id):
+        self.User_Order.append(user_id)
+
+class DB_Manager:
+    def Check_Account_Details(self, username, password):
+        pass
+    
+    def Equipment_querry(self, searrch_term):
+        pass
+    
+    def Check_Equipment_Status(self, equipment_id):
+        pass
+    
+    def Store_Request(self, request: Request):
+        pass
+
+class Screen:
+    
+    def display(self):
+        pass
+
+class User_Main_Screen(Screen):
+    
+    def Manage_Equipment(self):
+        pass
+
+class Admin_Main_Screen(Screen):
+    
+    def Create_Account(self):
+        pass
+    
+    def Account_Created_Message(self):
+        pass
+
+class Search_Screen(Screen):
+    
+    def Search_Equipment(self):
+        pass
+    
+class Search_Results_Screen(Screen):
+    
+    def Display_Results(self, results):
+        pass
+    
+    def Select_Equipment(self):
+        pass
+    
+class Unavailable_Equipment_Screen(Screen):
+    
+    def Add_To_Queue(self):
+        pass
+
+class Request_Screen(Screen):
+    
+    def Confirm_Rquest(self):
+        pass
+
+class Account_Creation_Screen(Screen):
+    
+    def Enter_Account_Details(self):
+        pass
+    
+    def Failure_Message():
+        print("Account creation failed. Please try again.")
+
+class Log_Entry:
+    def __init__(self):
+        pass
